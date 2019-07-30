d84909a3a6cb1791cd90ac01b73cc5d7e0067aba
MehriB <mehri.baniasadi92@gmail.com>
2019-07-30 08:36:46 +0200

    plot to have vta

--- plot_efield.m
+++ plot_efield.m
@@ -1,74 +1,71 @@
+function [hGroupField] = plot_efield(Efield,xg,yg,zg,plot_choice)
 
-function [hGroupElec, hTarget] = plot_el_target(perc,elfv,atlases,target,side)
-ea_dispt('Plot the Electrode and the target ...')
+if strcmp(plot_choice,'vta_efield')
+    
+ea_dispt('plot Efield...')
+% visualize Efield
+min_e=min(Efield(:));
+max_e=max(Efield(:));
+
+range_e=max_e-min_e;
+step_e=range_e/63;
+
+E_layers = min_e:step_e:max_e;
+cut = 1:100;
+
+data = Efield(:,:,cut);
+X=xg;
+Y=yg;
+Z=zg;
+
+cmap = colormap(autumn);
+hGroupField = hggroup();
+h = {};
 
 
+ for i = 1:length(E_layers)
+     
+     if E_layers(i) >200
+         
+        s = isosurface(X(:,:,cut), Y(:,:,cut), Z(:,:,cut), permute(data, [2 1 3]) > E_layers(i) );
 
-% visualize Electrode
-hGroupElec = hggroup();
+        h{end+1} = patch(s, 'EdgeColor', 'none', 'FaceColor', cmap(i,:) , 'FaceAlpha', 0.1, 'Parent', hGroupField);
+         
+     else
+        s = isosurface(X(:,:,cut), Y(:,:,cut), Z(:,:,cut), permute(data, [2 1 3]) > E_layers(i) );
 
-perc(9)=0;
-hold on;
-for i=1:length(elfv)
-    if(ismember(i, [1 2 3 4 5 6 7 8 9]))
-        if perc(i)>0
-           patch(elfv(i), 'FaceColor', [1 0.1 0.1] , 'EdgeColor', 'none', 'Parent', hGroupElec);
-        else
-           patch(elfv(i), 'FaceColor', rgb('dimgray'), 'EdgeColor', 'none', 'Parent', hGroupElec);
-        end
-    else
-        patch(elfv(i), 'FaceColor', rgb('lightgray'), 'EdgeColor', 'none', 'Parent', hGroupElec);
-    end
-end
+        h{end+1} = patch(s, 'EdgeColor', 'none', 'FaceColor', cmap(i,:), 'FaceAlpha', 0.07, 'Parent', hGroupField);
 
+      end
+ end
  
-% visualize Target
+ ea_dispt('')
+ 
+elseif strcmp(plot_choice,'vta')
+    ea_dispt('plot vta...')
+% visualize Efield
 
-hTarget  = [];
-if strcmp(target,'stn')
-    hold on;
-    if side == 1
-            hTarget = patch(atlases.fv{1,1}, 'FaceColor', [0 0.75 0.75], 'EdgeColor', 'none', 'FaceAlpha', 0.5);
-            material dull
-       elseif side == 2
-            hTarget = patch(atlases.fv{1,2}, 'FaceColor', [0 0.75 0.75], 'EdgeColor', 'none', 'FaceAlpha', 0.5);
-            material dull
-    end
-    
-elseif strcmp(target,'stn_motor')
-      hold on;
-        if side == 1
-            hTarget = patch(atlases.fv{2,1}, 'FaceColor', [0 0.5 0.62], 'EdgeColor', 'none', 'FaceAlpha', 0.5);
-            material dull
-            
-       elseif side == 2
-            hTarget = patch(atlases.fv{2,2}, 'FaceColor', [0 0.5 0.62], 'EdgeColor', 'none', 'FaceAlpha', 0.5);
-            material dull
-        end
-        
-elseif strcmp(target,'vim')
-      hold on;
-        if side == 1
-            hTarget = patch(atlases.fv{18,1}, 'FaceColor', [0 0.5 0.62], 'EdgeColor', 'none', 'FaceAlpha', 0.5);
-            material dull
-            
-       elseif side == 2
-            hTarget = patch(atlases.fv{18,2}, 'FaceColor', [0 0.5 0.62], 'EdgeColor', 'none', 'FaceAlpha', 0.5);
-            material dull
-        end
-    
+E_layers = 200;
+cut = 1:100;
+
+data = Efield(:,:,cut);
+X=xg;
+Y=yg;
+Z=zg;
+
+cmap = colormap(autumn);
+hGroupField = hggroup();
+h = {};
+
+
+        s = isosurface(X(:,:,cut), Y(:,:,cut), Z(:,:,cut), permute(data, [2 1 3]) > E_layers );
+
+        h{end+1} = patch(s, 'EdgeColor', 'none', 'FaceColor', cmap(1,:) , 'FaceAlpha', 0.25, 'Parent', hGroupField);
+         
+ 
+ ea_dispt('')
+ 
+ 
 end
-camlight left;
-lighting gouraud
-daspect([1 1 1]);
-addToolbarExplorationButtons(gcf);
-ea_dispt('');
-set(gca,'color',[0.9 0.9 0.9]);
-set(gca,'XTick',[]);
-set(gca,'Xcolor',[0.9 0.9 0.9])
-set(gca,'YTick',[]);
-set(gca,'Ycolor',[0.9 0.9 0.9])
-set(gca,'ZTick',[]);
-set(gca,'Zcolor',[0.9 0.9 0.9])
-title('Estimated Efield');
-end
+ 
+ 
